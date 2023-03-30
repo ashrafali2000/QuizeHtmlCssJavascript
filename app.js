50634cc06c4784ce40d2062c4c4ab553c66472b5
Ashraf-ali-2000 <ali4282271@gmail.com>
2023-03-30 16:06:47 +0500

    addSimpleQuize

---
+++ app.js
@@ -0,0 +1,683 @@
+// index.html
+function checkRadio() {
+    var htm = document.getElementById("html");
+    var cs = document.getElementById("css");
+    var java = document.getElementById("javascript");
+    if (htm.checked) {
+        window.open("Html/html1.html");
+    }
+    else if (cs.checked) {
+        window.open("Css/css1.html");
+    }
+    else if (java.checked) {
+        window.open("Javascript/javascript1.html");
+    }
+}
+
+//Html
+//html-1
+function checkRadiohtml() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("html2.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("html2.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("html2.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("html2.html", "_self");
+    }
+}
+
+//html-2
+function checkRadiohtml2() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("html3.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("html3.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("html3.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("html3.html", "_self");
+    }
+}
+//html-3
+function checkRadiohtml3() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("html4.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("html4.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("html4.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("html4.html", "_self");
+    }
+}
+//html-4
+function checkRadiohtml4() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("html5.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("html5.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("html5.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("html5.html", "_self");
+    }
+}
+//html-5
+function checkRadiohtml5() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("html6.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("html6.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("html6.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("html6.html", "_self");
+    }
+}
+//html-6
+function checkRadiohtml6() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("html7.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("html7.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("html7.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("html7.html", "_self");
+    }
+}
+//html-7
+function checkRadiohtml7() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("html8.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("html8.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("html8.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("html8.html", "_self");
+    }
+}
+//html-8
+function checkRadiohtml8() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("html9.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("html9.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("html9.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("html9.html", "_self");
+    }
+}
+//html-9
+function checkRadiohtml9() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("html10.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("html10.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("html10.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("html10.html", "_self");
+    }
+}
+
+//  CSS
+
+// css-1
+function checkRadiocss1() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("css2.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("css2.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("css2.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("css2.html", "_self");
+    }
+}
+//css-2
+function checkRadiocss2() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("css3.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("css3.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("css3.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("css3.html", "_self");
+    }
+}
+//css-3
+function checkRadiocss3() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("css4.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("css4.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("css4.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("css4.html", "_self");
+    }
+}
+//css-4
+function checkRadiocss4() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("css5.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("css5.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("css5.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("css5.html", "_self");
+    }
+}
+//css-5
+function checkRadiocss5() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("css6.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("css6.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("css6.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("css6.html", "_self");
+    }
+}
+//css-6
+function checkRadiocss6() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("css7.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("css7.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("css7.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("css7.html", "_self");
+    }
+}
+//css-7
+function checkRadiocss7() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("css8.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("css8.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("css8.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("css8.html", "_self");
+    }
+}
+//css-8
+function checkRadiocss8() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("css9.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("css9.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("css9.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("css9.html", "_self");
+    }
+}
+//css-9
+function checkRadiocss9() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("css10.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("css10.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("css10.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("css10.html", "_self");
+    }
+}
+
+// JAVASCRIPT
+//javascript-1
+function checkRadiojavascript1() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("javascript2.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("javascript2.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("javascript2.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("javascript2.html", "_self");
+    }
+}
+//javascript-2
+function checkRadiojavascript2() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("javascript3.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("javascript3.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("javascript3.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("javascript3.html", "_self");
+    }
+}
+//javascript-3
+function checkRadiojavascript3() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("javascript4.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("javascript4.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("javascript4.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("javascript4.html", "_self");
+    }
+}
+//javascript-4
+function checkRadiojavascript4() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("javascript5.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("javascript5.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("javascript5.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("javascript5.html", "_self");
+    }
+}
+//javascript-5
+function checkRadiojavascript5() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("javascript6.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("javascript6.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("javascript6.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("javascript6.html", "_self");
+    }
+}
+//javascript-6
+function checkRadiojavascript6() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("javascript7.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("javascript7.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("javascript7.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("javascript7.html", "_self");
+    }
+}
+//javascript-7
+function checkRadiojavascript7() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("javascript8.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("javascript8.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("javascript8.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("javascript8.html", "_self");
+    }
+}
+//javascript-8
+function checkRadiojavascript8() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("javascript9.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("javascript9.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("javascript9.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("javascript9.html", "_self");
+    }
+}
+//javascript-9
+function checkRadiojavascript9() {
+    var abdul = document.getElementById("abdulrehman");
+    var zaid = document.getElementById("zaid");
+    var ashraf = document.getElementById("ashraf");
+    var jawan = document.getElementById("jawan");
+    if (abdul.checked) {
+        window.open("javascript10.html", "_self");
+    }
+    else if (zaid.checked) {
+        window.open("javascript10.html", "_self");
+    }
+    else if (ashraf.checked) {
+        window.open("javascript10.html", "_self");
+    }
+    else if (jawan.checked) {
+        window.open("javascript10.html", "_self");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Html
+//Q-01
+// function checkRadiohtml(){
+//     var Abdulrehman = document.getElementById("abdulrehman");
+//     var Zaid_ali = document.getElementById("zaid");
+//     var Ashraf_ali = document.getElementById("ashraf");
+//     var jawan_bacha = document.getElementById("jawan");
+//     var result = "";
+//     var count = 0;
+//     if (Abdulrehman.checked) {
+//         result = "false";
+//         count += 1;
+//     }
+//     else if (Zaid_ali.checked) {
+//         result = "false";
+//         count += 1;
+//     }
+//     else if (Ashraf_ali.checked) {
+//         result = "false";
+//         count += 1;
+//     }
+//     else if (jawan_bacha.checked) {
+//         result = "true";
+//         count += 1;
+//     }
+//  else {
+// window.open("Html/html1.html");
+//  }
+// }
+// //Q-02
+// function checkRadiohtml(){
+//     var Abdulrehman = document.getElementById("abdulrehman");
+//     var Zaid_ali = document.getElementById("zaid");
+//     var Ashraf_ali = document.getElementById("ashraf");
+//     var jawan_bacha = document.getElementById("jawan");
+//     var result = "";
+//     if (Abdulrehman.checked) {
+//         result = "false";
+//     }
+//     else if (Zaid_ali.checked) {
+//         result = "false";
+//     }
+//     else if (Ashraf_ali.checked) {
+//         result = "false";
+//     }
+//     else if (jawan_bacha.checked) {
+//         result = "true";
+//     }
+// }
+// //Q-03
+// function checkRadiohtml(){
+//     var Abdulrehman = document.getElementById("abdulrehman");
+//     var Zaid_ali = document.getElementById("zaid");
+//     var Ashraf_ali = document.getElementById("ashraf");
+//     var jawan_bacha = document.getElementById("jawan");
+//     var result = "";
+//     if (Abdulrehman.checked) {
+//         result = "false";
+//     }
+//     else if (Zaid_ali.checked) {
+//         result = "false";
+//     }
+//     else if (Ashraf_ali.checked) {
+//         result = "false";
+//     }
+//     else if (jawan_bacha.checked) {
+//         result = "true";
+//     }
+// }
